147b2313818dd0469348095985c8039a58c46736
connorbottone <42557300+connorbottone@users.noreply.github.com>
2022-12-07 22:46:59 -0500

    addde java need to finish list itmes function stared

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ https://getbootstrap.com/docs/4.0/components/jumbotron/
 https://getbootstrap.com/docs/4.0/components/forms/
 https://codepen.io/derekjp/pen/yyVWOq
 https://codepen.io/evanjpeterson/pen/MBppQP
+https://pixnio.com/media/sunrise-woodland-trees-dawn-nature
+
+
+
--- asstes/my.js
+++ asstes/my.js
@@ -1,12 +1,43 @@
-var apiHttps =  'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=9ae65e07b51d7c7d559b20d779c9b073'
-//grab all files from html that will be inouting and diplaying data
 
 
-fetch(apiHttps)
-.then(function(response){
-    return response.json();
 
+// const nflApi = 'https://api.openweathermap.org/data/2.5/forecast?q={cityname}&appid=9ae65e07b51d7c7d559b20d779c9b073'
+//     const response = await fetch(nflApi);
+//     const data = await response.json();
+//     const { items } = data;
+
+
+//     for (var i = 0; i < items.length; i++) {
+//         var team = items[i];
+        
+
+//         fetch(team.$ref)
+//         .then(function (response) {
+//             // console.log(team.$ref); //console logs the team links
+//             // console.log(response);
+//             return response.json();
+//         }) .then(function(data) {
+//             const { date } = data;
+//             console.log(date); //returns the data from the team links
+        
+ var x = document.getElementById("")
+        var v = document.getElementById("#city").val();
+$('#btn1').on("click", function () {
+     
+    localStorage.setItem('City', $("#city").val());
+   
+
+console.log(city)
+listSearchedCity()
 })
-.then(function(data) {
-    console.log(data)
-});
+function listSearchedCity() {
+    
+var repoEl = document.createElement('p');
+    repoEl.classList = 'list-item flex-row justify-space-between align-center';
+    repoEl.textContent = v;
+
+    x.appendChild(repoEl);
+
+
+}
+//when the user clciks the botton there choices will be set to an orderder list on this side of middle display.
